93f0f5aeb46429aa2689cb9ff2856fb1ad3ceead
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-07-02 17:23:04 -0300

    fea: nodemailer working

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,8 +1,40 @@
-//export async function GET(request) {
-//  return new Response("Hello!");
-//}
+//Aqui ele recebe os dados do servidor e envia o email. Se for realizar validações do email, aqui é o lugar.
+import { NextResponse } from "next/server";
+import nodemailer from "nodemailer";
 
-export default function handler(req, res) {
-  console.log(req.body);
-  res.status(200).json({ name: "john doe" });
+export async function POST(request: Request) {
+  const { name, email, message } = request.body;
+
+  const user = process.env.EMAIL;
+  const pass = process.env.EMAIL_PASS;
+
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, //updrade later with STARTTLS
+    auth: {
+      user: user,
+      pass,
+    },
+  });
+
+  try {
+    const mail = await transporter.sendMail({
+      from: user,
+      to: "vitorschelb.contact@gmail.com",
+      replyTo: email,
+      subject: `Contact form submission from ${name}`,
+      html: `
+      <p>Name: ${name} </p>
+      <p>Email: ${email} </p>
+      <p>Message: ${message} </p>`,
+    });
+
+    console.log("Message sent:", mail.messageId);
+
+    return NextResponse.json({ message: "success" });
+  } catch (error) {
+    console.log(error);
+    NextResponse.json({ message: "Nao foi possivel" });
+  }
 }
--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { ChangeEvent, useState } from "react";
-import { sendContactForm } from "../lib/api";
 
 const initValues = { name: "", email: "", subject: "", message: "" };
 const initState = { values: initValues };
@@ -8,23 +7,41 @@ const initState = { values: initValues };
 type InputChangeEvent = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
 
 export default function ContactForm() {
-  const [state, setState] = useState(initState);
-  const { values } = state;
-  const handleChange = ({ target }: InputChangeEvent) =>
-    setState((prev) => ({
-      ...prev,
-      values: {
-        ...prev.values,
-        [target.name]: target.value,
-      },
-    }));
+  const [loading, setLoading] = useState(false)
+  
+  async function handleSubmit(event: any) {
+    event.preventDefault();
+    setLoading(true)
 
-  const onSubmit = async () => {
-    setState((prev) => ({
-      ...prev,
-    }));
-    await sendContactForm(values)
-  };
+    const data = {
+      name: String(event.target.name.value),
+      email: String(event.target.name.value),
+      message: String(event.target.name.value),
+    };
+
+    // Aqui envia para o servidor
+    const response = await fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+      console.log("Message sent"); //Redirecionar para uma pagina agradecendo o contato, ou mostrar o alerta de sucesso.
+      setLoading(false)
+      //reset the form
+      event.target.name.value = ""
+      event.target.email.value = ""
+      event.target.message.value = ""
+    }
+
+    if (!response.ok) {
+      console.log("Message not send");
+      setLoading(false)
+    }
+  }
 
   return (
     <section className="bg-white dark:bg-gray-900">
@@ -32,7 +49,7 @@ export default function ContactForm() {
         <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
           Contact Us
         </h2>
-        <form className="">
+        <form onSubmit={handleSubmit}>
           <div>
             <div>
               <label
@@ -48,8 +65,6 @@ export default function ContactForm() {
                 className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="Your Name"
                 required
-                defaultValue={values.name}
-                onChange={handleChange}
               />
             </div>
             <div className="sm:col-span-2">
@@ -65,27 +80,9 @@ export default function ContactForm() {
                 className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
                 placeholder="name@flowbite.com"
                 required
-                defaultValue={values.email}
-                onChange={handleChange}
               />
             </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="subject"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-              >
-                subject
-              </label>
-              <input
-                type="text"
-                id="subject"
-                className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                placeholder="name@flowbite.com"
-                required
-                defaultValue={values.subject}
-                onChange={handleChange}
-              />
-            </div>
+
             <label
               htmlFor="message"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
@@ -97,15 +94,13 @@ export default function ContactForm() {
               rows={6}
               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
               placeholder="Leave a comment..."
-              defaultValue={values.message}
-              onChange={handleChange}
             ></textarea>
           </div>
           <button
             //Fazer o Disabled e o isLoading
             type="submit"
-            className="py-3 disabled:bg-red-500 px-5 text-sm font-medium text-center text-white rounded-lg bg-gray-700 sm:w-fit hover:bg-black focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-slate-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
-            onClick={onSubmit}
+            disabled={loading}
+            className="py-3 disabled:bg-gray-400 disabled:text-gray-100 px-5 text-sm font-medium text-center text-white rounded-lg bg-gray-700 sm:w-fit hover:bg-black focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-slate-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
           >
             Send message
           </button>
