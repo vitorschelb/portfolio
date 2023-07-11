9d5c5de1b5fa7de77639c18726fd1a895f32b6cf
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-07-10 22:53:47 -0300

    ref: CSS classes ref

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -2,32 +2,33 @@
 
 import Link from "next/link";
 import MobileNavbar from "./MobileNavbar";
-import { SiGithub, SiLinkedin } from "react-icons/si";
 import { navList } from "../shared/Data";
+import { SiGithub, SiLinkedin } from "react-icons/si";
 
 export default function Sidebar() {
   return (
-    <nav className="">
+    <nav>
       {/* Desktop Menu */}
 
-      <div className="h-screen items-center p-3 bg-gray-three text-clean-white font-poppins font-medium text-center shadow-xl hidden lg:block lg:w-64">
-        <ul className="flex w-full flex-col items-center justify-center h-full">
+      <div className="h-screen hidden lg:flex lg:flex-col lg:w-64 items-center justify-center bg-gray-three text-clean-white font-poppins font-medium text-center shadow-xl">
+        <ul className="flex flex-col w-full">
           {navList.map((navItem, i) => (
-            <Link key={i} href={navItem.href} className="p-3 border-b w-full border-gray-two hover:text-gray-three hover:bg-white ease-in-out duration-500">
-              <li >
-                {navItem.label}
-              </li>
+            <Link
+              key={i}
+              href={navItem.href}
+              className="p-3 w-full border-b border-gray-two hover:text-gray-three hover:bg-white ease-in-out duration-500"
+            >
+              <li>{navItem.label}</li>
             </Link>
           ))}
         </ul>
-
-        <div className="flex justify-center mb-10 items-center text-2xl mt-8 gap-4 text-clean-white">
+        <div className="mt-10 flex gap-4 text-clean-white text-2xl">
           <Link href="/">
-            <SiGithub />
+            <SiGithub className="hover:text-gray-two ease-in-out duration-500" />
           </Link>
 
           <Link href="/">
-            <SiLinkedin />
+            <SiLinkedin className="hover:text-gray-two ease-in-out duration-500" />
           </Link>
         </div>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar";
 import { Metadata } from "next";
 import { PropsWithChildren } from "react";
 import PreLoader from "./components/PreLoader";
+import SideIcons from "./components/SideIcons";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
