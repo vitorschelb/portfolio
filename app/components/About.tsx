1d55a4500dd8ece64b3f6d6522200f2058849688
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-07-11 08:44:52 -0300

    fea: About text and ref

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,39 +1,27 @@
 "use client";
 import Image from "next/image";
-import home from "../../public/home.png";
 import me from "../../public/me.jpeg";
-import { AiFillCaretRight } from "react-icons/ai";
+import { aboutList } from "../shared/Data";
 
 export default function About() {
   return (
     <section className="grid-cols-1 h-screen grid px-8 bg-clean-white md:px-20 2xl:px-36 md:grid-cols-2 md:gap-10">
       <div className="flex flex-col justify-center">
         <div className="flex items-center justify-around mb-6">
-          <h1 className="tracking-super text-xl font-poppins font-bold mr-10 text-gray-three">ABOUT</h1>
+          <h1 className="tracking-super text-xl font-poppins font-bold mr-10 text-gray-three">
+            ABOUT
+          </h1>
           <span className="w-full border-gray-zero border-b" />
         </div>
-
         <ul>
-          <li className="flex mb-2 text-gray-zero">
-            <AiFillCaretRight className="mr-2 text-3xl text-gray-zero" />
-            Currently, I work in the development and mentoring team for
-            community projects at PodCodar.
-          </li>
-          <li className="flex mb-2 text-gray-zero">
-            <AiFillCaretRight className="mr-2 text-3xl text-gray-zero" />
-            Currently, I work in the development and mentoring team for
-            community projects at PodCodar.
-          </li>
-          <li className="flex mb-2 text-gray-zero">
-            <AiFillCaretRight className="mr-2 text-3xl text-gray-zero" />
-            Currently, I work in the development and mentoring team for
-            community projects at PodCodar.
-          </li>
-          <li className="flex mb-2 text-gray-zero">
-            <AiFillCaretRight className="mr-2 text-3xl text-gray-zero" />
-            Currently, I work in the development and mentoring team for
-            community projects at PodCodar.
-          </li>
+          {aboutList.map((aboutItem, i) => (
+            <li
+              key={i}
+              className="flex mb-2 text-justify text-sm text-gray-zero before:content-['➤'] before:mr-2"
+            >
+              {aboutItem.text}
+            </li>
+          ))}
         </ul>
       </div>
 
--- app/shared/Data.ts
+++ app/shared/Data.ts
@@ -58,4 +58,21 @@ export const navList = [
   { label: "CONTACT", href: "/contact" },
 ];
 
+export const aboutList = [
+  {
+    text: "Currently, I work in the development and project mentoring team at PodCodar.",
+  },
+  {
+    text: "I perceive programming as a powerful tool for social transformation. My objective is to develop innovative solutions, streamline processes, and build technologies that can yield positive impacts in various areas of life.",
+  },
+  {
+    text: " Seek to apply good development practices, with emphasis on clean, readable code, version control and suitable testing approach.",
+  },
+  {
+    text: "   Master&apos;s degree in Theory of History and Digital Humanities, focusing on the impacts of technological transformations on the practices of remembering and forgetting among users in social media networks.",
+  },
+  {
+    text: "Seven years of classroom experience as an educator, I have effectively managed and supervised internship teams and extension projects.",
+  },
+];
 export default projectsData;
