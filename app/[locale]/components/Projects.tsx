8309be0d4752dc9dc52baa9a0070c241d261ae3e
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-08-06 21:50:34 -0300

    fix: space adjust

--- app/[locale]/components/Projects.tsx
+++ app/[locale]/components/Projects.tsx
@@ -5,8 +5,8 @@ import ProjectsCarousel from "./ProjectsCarousel";
 
 export default function Projects() {
   return (
-    <section className="flex flex-col justify-center h-screen bg-clean-white px-8 md:px-20 2xl:px-36">
-      <div className="flex items-center justify-around 2xl:mb-10">
+    <section className="flex flex-col justify-center h-screen bg-clean-white gap-6 px-8 md:px-20 2xl:px-36">
+      <div className="flex items-center justify-around">
         <h1 className="tracking-super text-xl font-bold font-poppins mr-10 text-gray-three">
           PROJECTS
         </h1>
