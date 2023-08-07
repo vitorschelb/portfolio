26509e5a4625a48a891a8f237aeec59eea873b7e
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-08-07 15:59:39 -0300

    ref: contact title space

--- app/[locale]/components/ContactForm.tsx
+++ app/[locale]/components/ContactForm.tsx
@@ -67,7 +67,7 @@ export default function ContactForm() {
       <section className="flex flex-col justify-center items-center h-screen bg-clean-white px-8 md:px-20 2xl:px-36">
         <div>
           <div className="flex items-center mb-6">
-            <h1 className="tracking-widest text-xl font-poppins font-bold text-gray-three">
+            <h1 className="tracking-widest text-xl mr-10 font-poppins font-bold text-gray-three">
               {t("title")}
             </h1>
             <span className="w-full border-gray-zero border-b" />
