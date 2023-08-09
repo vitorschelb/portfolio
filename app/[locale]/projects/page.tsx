cd3e3418478b13c4c09d91260c60f6a703b78c8a
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-08-09 09:26:06 -0300

    fix: remove props slide from provider

--- app/[locale]/projects/page.tsx
+++ app/[locale]/projects/page.tsx
@@ -1,12 +1,11 @@
 import { PageTransition } from "../components/PageTransition";
 import Projects from "../../modules/Projects";
 import { ProjectsProvider } from "@/app/contexts/ProjectsContext";
-import projectsData from "@/app/shared/Data";
 
 export default function PageProjects() {
   return (
     <main>
-      <ProjectsProvider slides={projectsData.length}>
+      <ProjectsProvider >
         <PageTransition>
           <Projects/>
         </PageTransition>
