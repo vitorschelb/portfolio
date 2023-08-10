2d03cc8dd34ab89ff46a50943ed9bf5e96cd0c26
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-08-10 12:42:52 -0300

    fix: test open first modal in some devices

--- app/[locale]/components/ProjectsModal.tsx
+++ app/[locale]/components/ProjectsModal.tsx
@@ -23,6 +23,7 @@ export default function ProjectsModal({projectIndex}: ProjectsModalProps) {
   const openModalWithIndex = () => {
     setCurrentIndex((projectIndex ?? currentIndex));
     onOpen();
+    alert(currentIndex)
   };
 
   return (
@@ -31,7 +32,7 @@ export default function ProjectsModal({projectIndex}: ProjectsModalProps) {
         onClick={onOpen}
         className="text-[#CFD3D6] text-8xl group-hover:animate-float hidden lg:block"
       />
-      <span>
+      <span onTouchStart={() => openModalWithIndex()} role="button" onClick={() => openModalWithIndex()}>
         <div className="absolute bottom-0 flex justify-center items-center right-0 ls:mr-8 bg-[#CFD3D6] m-2 p-1 rounded-full lg:hidden">
           <span>
             <ImEye onTouchStart={() => openModalWithIndex()} role="button" onClick={() => openModalWithIndex()} className="text-2xl text-gray-three ls:text-4xl animate-pulse ease-in-out duration-1000" />
