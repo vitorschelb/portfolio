9eeeed42f5b8f541bce9a8b53439163a27d54f56
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-08-10 11:03:21 -0300

    ref: test

--- app/[locale]/components/ProjectMobileCarousel.tsx
+++ app/[locale]/components/ProjectMobileCarousel.tsx
@@ -52,8 +52,8 @@ export default function ProjectMobileCarousel() {
             <h1 className="font-poppins text-xs font-semibold text-gray-three tracking-widest shadow-sm bg-[#CFD3D6] absolute m-2 p-1">
             {t(`${projectItem.id}.title`)}
             </h1>
-          </div>
             <ProjectsModal projectIndex={index} />
+          </div>
         </SwiperSlide>
       ))}
 
