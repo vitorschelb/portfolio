b6db4b72c7723e6bb12857ce9161572f26d9541b
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-08-09 21:46:18 -0300

    fea: dynamic mobile carousel and modal

--- app/[locale]/components/ProjectsModal.tsx
+++ app/[locale]/components/ProjectsModal.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import {
-  Button,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -11,12 +10,31 @@ import {
 } from "@chakra-ui/react";
 import ProjectsModalContent from "./ProjectsModalContent";
 import { ImEye } from "react-icons/im";
+import { AddIcon } from "@chakra-ui/icons";
+import { useProjects } from "@/app/contexts/ProjectsContext";
 
-export default function ProjectsModal() {
+type ProjectsModalProps = {
+  projectIndex?: number;
+};
+
+export default function ProjectsModal({projectIndex}: ProjectsModalProps) {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const { setCurrentIndex, currentIndex} = useProjects();
+
+  const openModalWithIndex = () => {
+    setCurrentIndex((projectIndex ?? currentIndex));
+    onOpen();
+  };
+
   return (
     <>
-      <ImEye onClick={onOpen} className="text-[#CFD3D6] text-8xl group-hover:animate-float" />
+      <ImEye
+        onClick={onOpen}
+        className="text-[#CFD3D6] text-8xl group-hover:animate-float hidden lg:block"
+      />
+      <div className="absolute bottom-0 right-0 bg-[#CFD3D6] m-2 p-1 rounded-full lg:hidden">
+        <AddIcon onClick={() => openModalWithIndex()} className="text-2xl text-gray-three animate-pulse ease-in-out duration-1000" />
+      </div>
       <Modal
         size="xl"
         scrollBehavior={"inside"}
@@ -24,12 +42,19 @@ export default function ProjectsModal() {
         onClose={onClose}
       >
         <ModalOverlay />
-        <ModalContent borderRadius="none">
+        <ModalContent borderRadius="none"
+        >
           <ModalBody padding={0}>
-            <ProjectsModalContent />
+            <ProjectsModalContent  />
           </ModalBody>
-   
-            <ModalCloseButton />
+          <ModalCloseButton
+            boxShadow={"sm"}
+            bgColor="blackAlpha.800"
+            textColor={"white"}
+            borderRadius="none"
+            padding={0}
+            margin={0}
+          />
         </ModalContent>
       </Modal>
     </>
