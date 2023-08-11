1bf3844347b0c5e2df10687552e5cc78ce4d3d00
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-08-11 20:19:28 -0300

    Fea: Uploads ecommerce project

--- app/[locale]/components/ProjectsModal.tsx
+++ app/[locale]/components/ProjectsModal.tsx
@@ -9,10 +9,11 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 import ProjectsModalContent from "./ProjectsModalContent";
+
 import { ImEye } from "react-icons/im";
+
 import { useProjects } from "@/app/contexts/ProjectsContext";
 import { useTranslations } from "next-intl";
-
 
 type ProjectsModalProps = {
   index?: number;
@@ -20,28 +21,30 @@ type ProjectsModalProps = {
 
 export default function ProjectsModal({ index }: ProjectsModalProps) {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const {setCurrentIndex} = useProjects()
+  const { setCurrentIndex } = useProjects();
   const t = useTranslations("ProjectsCard");
 
   const openWithMobileIndex = () => {
-    setCurrentIndex(index ?? 0)
-    onOpen()
-   }
-
+    setCurrentIndex(index ?? 0);
+    onOpen();
+  };
 
   return (
     <>
       <ImEye
         onClick={onOpen}
+        tabIndex={0}
         className="text-[#CFD3D6] text-8xl group-hover:animate-float hidden lg:block"
       />
 
-        <button
-          onClick={openWithMobileIndex}
-          className="font-raleway mt-2 font-semibold w-full text-clean-white flex justify-center bg-gray-three p-2 shadow-md lg:hidden">
-            {t("Common.detailsButton")}
-        </button>
-  
+      <button
+        onClick={openWithMobileIndex}
+        tabIndex={0}
+        className="font-raleway mt-2 font-semibold w-full text-clean-white flex justify-center bg-gray-three p-2 shadow-md lg:hidden"
+      >
+        {t("Common.detailsButton")}
+      </button>
+
       <Modal
         size="xl"
         scrollBehavior={"inside"}
--- app/shared/Data.ts
+++ app/shared/Data.ts
@@ -1,7 +1,9 @@
 import { StaticImageData } from "next/image";
+import { z } from "zod";
+
 import podcodar from "../../public/podcodar.webp";
 import portfolio from "../../public/portfolio.webp";
-import { z } from "zod";
+import ecommerce from "../../public/ecommerce.webp";
 
 export type Props = {
   children: React.ReactNode;
@@ -18,7 +20,7 @@ export const feedbackSchema = z.object({
 });
 
 export type Project = {
-  id: "my-portfolio" | "podcodar-app";
+  id: "my-portfolio" | "podcodar-app" | "ecommerce-app";
   image: StaticImageData;
   repoURL: string;
   deployURL: string;
@@ -40,11 +42,20 @@ export const projectsData: Project[] = [
   {
     id: "my-portfolio",
     image: portfolio,
-    repoURL: "https://github.com/vitorschelb/portfolio",
+    repoURL: "https://https://github.com/vitorschelb/ecommerce-app",
     deployURL: "https://vitorschelb.dev",
     technologies:
       "Next TypeScript Tailwind Framer-Motion ChakraUI Next-intl Zod Nodemailer",
-    about: "PORTIFAS",
+    about: "Portfiolio",
+  },
+  {
+    id: "ecommerce-app",
+    image: ecommerce,
+    repoURL: "https://github.com/vitorschelb/portfolio",
+    deployURL: "https://ecommerce-app-9lwc.vercel.app/",
+    technologies:
+      "Next TypeScript Tailwind Radix Sanity UseShoppingCart Stripe",
+    about: "Ecommerce",
   },
   {
     id: "podcodar-app",
@@ -53,7 +64,7 @@ export const projectsData: Project[] = [
     deployURL: "https://github.com/podcodar/app",
     technologies:
       "Next TypeScript NextAuth Prisma Docker Tailwind-Styled-Components Zod",
-    about: "PODCODAR",
+    about: "Podcodar",
   },
 ];
 
