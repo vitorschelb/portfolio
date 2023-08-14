819d7f25b375fef2e12f57ec29c2d3695744678f
vitoraiala@gmail.com <vitoraiala@gmail.com>
2023-08-14 15:42:21 -0300

    ref: menu on right

--- app/[locale]/components/MobileNavBar.tsx
+++ app/[locale]/components/MobileNavBar.tsx
@@ -8,8 +8,8 @@ export default function MobileNavBar() {
   return (
     <ChakraProvider>
       <nav className="lg:hidden z-10 flex justify-between items-center absolute h-30 w-screen top-0 bg-clean-white shadow-sm">
-        <MobileSideBar />
         <ToggleLanguageMobile  />
+        <MobileSideBar />
       </nav>
     </ChakraProvider>
   );
